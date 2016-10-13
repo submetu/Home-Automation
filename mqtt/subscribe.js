13264bf3638f70590cac689fe7ead39cdc2ddc90
M Subhan Naeem <sub.metu@gmail.com>
2016-10-13 19:24:59 +0300

    added place for pir sensor and coding for getting the pir values on the topi pir

--- mqtt/subscribe.js
+++ mqtt/subscribe.js
@@ -1,12 +1,14 @@
 var mqttServer = require('./mqtt_server');
 
-function subscribe(topic){
+function subscribe(topic,callback){
 	//get the 'client' variable from mqtt_server module
 	mqttServer(function(err,client){
 		client.subscribe(topic,function(){
 		 	client.on('message', function (topic, message,packet) {
-		 	  // console.log(packet);
-			  console.log('The message "'+ message.toString() + '" came on the topic "'+ topic.toString()+'"');
+		 		callback(topic,message,packet)
+			  	//add callback to give others access to topic,message,packet
+		 	  	// console.log(packet);
+			  	// console.log('The message "'+ message.toString() + '" came on the topic "'+ topic.toString()+'"');
 			});
 		});
 	});
--- wesbsockets/socket_server.js
+++ wesbsockets/socket_server.js
@@ -2,8 +2,14 @@ function socketapp(server){
 	var io   = require('socket.io').listen(server);
 	var publish = require('../mqtt/publish');
 	var subscribe = require('../mqtt/subscribe');
+	var subscriptions = require('../mqtt/subscriptions');
  	
-	io.sockets.on('connection', function (socket) {  
+	io.sockets.on('connection', function (socket) { 
+		//add 'subscriptions.js' file to 'mqtt' folder and subscribe to everything required there
+		//call the function of subscriptions.js here and pass on 'io.sockets'\
+		//then use io.socket events in subscribe.js file to trigger listeners on the client side app.js
+		//In subscriptions.js publish.js can also be called to publish counter events to all the clients
+		subscriptions(io.sockets);
 	    console.log("Socket ID: " + socket.id);
 	    console.log('Wating for inputs ...');
 		// ledHandler(socket,inits.led,io.sockets);
