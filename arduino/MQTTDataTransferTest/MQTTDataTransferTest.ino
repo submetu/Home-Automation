56bd01c563e70df9a66f117de2dac8b13d3d989a
Subhan <sub.metu@gmail.com>
2016-10-19 18:54:52 +0300

    Update MQTTDataTransferTest.ino

--- arduino/MQTTDataTransferTest/MQTTDataTransferTest.ino
+++ arduino/MQTTDataTransferTest/MQTTDataTransferTest.ino
@@ -1,121 +1,108 @@
-/*
-  Basic MQTT example
-
-  This sketch demonstrates the basic capabilities of the library.
-  It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic"
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-
-  It will reconnect to the server if the connection is lost using a blocking
-  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
-  achieve the same result without blocking the main loop.
-
-*/
-
-#include <SPI.h>
-#include <Ethernet.h>
+#include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-int lightstate = 0;
-unsigned long temperaturetime = 0;
-int val;
-
 // Update these with values suitable for your network.
-byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
-IPAddress ip(192, 168, 1, 99);
-IPAddress server(192, 168, 1, 43);
+
+const char* ssid = "TTNET_ZyXEL_ACEA";
+const char* password = "8F231265670E2";
+const char* mqtt_server = "192.168.1.43";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long earlier = 0;
+char msg[50];
+int value = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
 
 void callback(char* topic, byte* payload, unsigned int length) {
+  char copiedstring[120] = {0};
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
+
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
+    copiedstring[i] = (char)payload[i];
   }
+  copiedstring[length] = '\0';
   Serial.println();
 
-  if (!strcmp(topic, "on"))
-    lightstate = 1;
-  else if (!strcmp(topic, "off"))
-    lightstate = 0;
-}
 
-EthernetClient ethClient;
-PubSubClient client(ethClient);
+
+  if (!strcmp(copiedstring, "1")) {
+    digitalWrite(13, LOW);
+    Serial.println("ONES");
+  }
+  else {
+    digitalWrite(13, HIGH);
+    Serial.println("ZEROS");
+  }
+
+
+}
 
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "Extension Cord";
     // Attempt to connect
-    if (client.connect("arduinoClient")) {
+    if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
+      client.publish("extout", "This is your extension cord speaking. Bow down to your automated overlords.");
       // ... and resubscribe
-      client.subscribe("on");
-      client.subscribe("off");
+      client.subscribe("extin");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
+      Serial.println(" try again in 1 seconds");
       // Wait 5 seconds before retrying
-      delay(5000);
+      delay(1000);
     }
   }
 }
 
-void switchlight() {
-  if (lightstate)
-    digitalWrite(6, HIGH);
-  else
-    digitalWrite(6, LOW);
-}
-
-void temperature() {
-
-  if (abs(millis() - temperaturetime) >= 5000) {
-    val = analogRead(A1);
-    float mv = ( val/1024.0)*5000; 
-    float cel = mv/10;
-    char result[5];
-    dtostrf(cel, 3, 2, result); 
-    client.publish("outTopic",result);
-    Serial.println(result);
-    temperaturetime = millis();
-  }
-
-  
-
-}
-
-
-
-void setup()
-{
-  Serial.begin(57600);
-
-  client.setServer(server, 1883);
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-
-  Ethernet.begin(mac, ip);
-  // Allow the hardware to sort itself out
-  delay(1500);
-  pinMode(6, OUTPUT);
-  pinMode(A1, INPUT);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH); //Make sure extension is powered off at startup//
 }
 
-void loop()
-{
+void loop() {
+
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
 
-  switchlight();
-
-  temperature();
-
-
+  long now = millis();
+  if (now - earlier > 10000) {
+    earlier = now;
+  }
 }
